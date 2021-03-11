6c3ce0dd444f888cd3d3b2aa568fe4af8409fba2
katenok <a.timin2003@mail.ru>
2021-03-11 22:08:58 +0300

    Added pics and index-file

---
+++ lesson4/public/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galery</title>
+    <link rel="stylesheet" href="style/main.css">
+</head>
+
+<body>
+    <main class="main">
+        <ul class="galery">
+            <li class="galery_item"><img class="pic_mini" src="img/mini/1143.jpg" alt="photo"></li>
+            <li class="galery_item"><img class="pic_mini" src="img/mini/5556494.jpg" alt="photo"></li>
+            <li class="galery_item"><img class="pic_mini" src="img/mini/584598.jpg" alt="photo"></li>
+            <li class="galery_item"><img class="pic_mini" src="img/mini/65494.jpg" alt="photo"></li>
+            <li class="galery_item"><img class="pic_mini" src="img/mini/8549.jpg" alt="photo"></li>
+            <li class="galery_item"><img class="pic_mini" src="img/mini/564654165.jpg" alt="photo"></li>
+        </ul>
+        <form method="post"></form>
+    </main>
+</body>
+
+</html>
